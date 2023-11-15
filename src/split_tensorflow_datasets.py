53fd0defeda5a42111a2a9af5684e807b514fed6
Dogyu Lee <123223901+dodorlee1210@users.noreply.github.com>
2023-11-15 18:15:57 -0500

    Added file for splitting data into tensorflow datasets

---
+++ src/split_tensorflow_datasets.py
@@ -0,0 +1,50 @@
+"""
+Split Data into Train, Test, Val
+"""
+
+import tensorflow as tf
+from keras.utils import image_dataset_from_directory
+import pathlib
+
+BATCH_SIZE = 32
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
+
+# Set directory to pull images from
+DATA_DIR = pathlib.Path('signatures_combined')
+
+paths = len(list(DATA_DIR.glob('*.png')))
+print(paths)
+# paths = [str(path).encode("utf-8") for path in DATA_DIR.glob('*.png')]
+
+
+# Split into train
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    DATA_DIR,
+    # labels='inferred',
+    validation_split=0.7,
+    subset="training",
+    seed=1107,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE,
+)
+
+# # Split into test
+# test_ds = tf.keras.utils.image_dataset_from_directory(
+#     DATA_DIR,
+#     validation_split=0.15,
+#     subset="testing",
+#     seed=1107,
+#     image_size=(IMG_HEIGHT, IMG_WIDTH),
+#     batch_size=BATCH_SIZE,
+# )
+
+# Split into validation
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    DATA_DIR,
+    validation_split=0.15,
+    subset="validation",
+    seed=1107,
+    image_size=(IMG_HEIGHT, IMG_WIDTH),
+    batch_size=BATCH_SIZE,
+)
