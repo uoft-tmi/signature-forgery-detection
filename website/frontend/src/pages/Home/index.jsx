27f0afb5d62929dbf3b522052362258fd3a7fb35
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2024-02-10 15:17:57 -0500

    image drag and drop functionality

--- website/frontend/src/pages/Home/index.jsx
+++ website/frontend/src/pages/Home/index.jsx
@@ -1,33 +1,77 @@
 import './style.css'
+import { useState, useRef } from 'react';
 
 const Home = () => {
+    const [previewImage, setPreviewImage] = useState(null);
+    const hiddenFileInput = useRef(null);
+
+    const handleDragOver = (event) => {
+        event.preventDefault();
+    }
+  
+    const handleDrop = (event) => {
+        event.preventDefault();
+        const file = event.dataTransfer.files[0];
+        if (file) {
+            setPreviewImage(URL.createObjectURL(file));
+        }
+    }
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            setPreviewImage(URL.createObjectURL(file));
+        }
+    }
+
+    const openFileInput = () => {
+        hiddenFileInput.current.click()
+    }
+
     return <>
         <main>
-            <div class="incontainer">
-                <div class = "body_gradient">
+            <div className="incontainer">
+                <div className = "body_gradient">
                 <h1>Sign of the Times</h1>
                 <h2>Detecting Forged Signatures with Machine Learning</h2>
                 <video width="720" height="480" controls>
                     <source src="" type="video/mp4"></source>
                     Your browser does not support the video tag.
                 </video>
-                <a href="#jump" class="button" id="try">Try it out!</a>
+                <a href="#jump" className="button" id="try">Try it out!</a>
                 </div>
 
                 <div>
-                <button type = "button" class = "button">Validate</button>
-                <div id="bottom">
-                    <label>
-                        <input type = "checkbox" class = "checkboxes" /> Model 1
-                    </label>
-                    <label>
-                        <input type = "checkbox" class = "checkboxes" /> Model 2
-                    </label>
-                    <label>
-                        <input type = "checkbox" class = "checkboxes" /> Select All
-                    </label>
-                    <p class = "textmargin">For more information about the models, click <a href = "about.html">here.</a></p>
-                </div>
+                    <div
+                        onDragOver={handleDragOver}
+                        onDrop={handleDrop}
+                        onClick={openFileInput}>
+                        {previewImage ? 
+                        (
+                            <img src={previewImage} alt='Preview'></img>
+                        ) :
+                        (
+                            <p>Drag & Drop or Browse</p>
+                        )}
+                        <input 
+                            type='file' 
+                            style={{display: 'none'}}
+                            onChange={handleFileChange}
+                            ref={hiddenFileInput}></input>
+                    </div>
+                    <button type = "button" className = "button">Validate</button>
+                    <div id="bottom">
+                        <label>
+                            <input type = "checkbox" className = "checkboxes" /> Model 1
+                        </label>
+                        <label>
+                            <input type = "checkbox" className = "checkboxes" /> Model 2
+                        </label>
+                        <label>
+                            <input type = "checkbox" className = "checkboxes" /> Select All
+                        </label>
+                        <p className = "textmargin">For more information about the models, click <a href = "about.html">here.</a></p>
+                    </div>
                 </div>
             </div>
         </main>
