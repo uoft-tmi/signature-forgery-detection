d85fd218853763713afecb14bdab401bf89aaf8c
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2024-05-11 19:52:42 -0400

    predict view draft

--- backend/sfmodels/views.py
+++ backend/sfmodels/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
+from PIL import Image
+import numpy as np
+import io
 
-# Create your views here.
+
+# TODO: figure out loading trained model into memory
+# want models to be a dict mapping names of models to the actual model
 
 class PredictView(APIView):
+    parser_classes = (FileUploadParser,)
+
     def post(self, request, *args, **kwargs):
-        return
+        if 'file' not in request.data:
+            return Response({'error': 'No file part'}, status=400)
+        
+        file = request.data['file']
+        
+        # Open the image using Pillow
+        image = Image.open(file)
+        
+        # Preprocess the image (resize, normalize, etc.)
+        image = image.resize((250, 250))
+        image = np.array(image) / 255.0    # Normalize pixel values
+        
+        # TODO: might need to reshape the image depending on the model input shape
+        image = np.expand_dims(image, axis=0)
+        
+        predictions = {}
+        for name in models:
+            predictions[name] = models[name].predict(image)
+        
+        return Response({'predictions': predictions})
