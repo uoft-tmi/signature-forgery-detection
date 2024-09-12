11de09d3abf5975f1794bd1058bdf7a0ab8610a2
dianzrong <dianrong2012@gmail.com>
2024-09-11 20:23:12 -0400

    updated settings

--- website/backend/backend/settings.py
+++ website/backend/backend/settings.py
@@ -126,5 +126,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://uoft-tmi.github.io/signature-forgery-detection/home"
 ]
